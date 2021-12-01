b99d0bca352005f6b1f489fcb50df57a842b5d3b
MinJoo Jeong <mjjeong999@gmail.com>
2021-12-01 09:44:40 +0900

    6. 검색, 삭제 기능 구현 및 출력 버튼 이벤트 구현

--- TelPhoneApp/MainForm.cs
+++ TelPhoneApp/MainForm.cs
@@ -29,19 +29,39 @@ namespace TelPhoneApp {
 
         private void btnSearch_Click(object sender, EventArgs e) {
             string name = txtSearch.Text;
-            if(name == "")
+            if (name == "")
                 return;
 
             txtSearch.Text = "";
             txtSearch.Focus();
 
             People tlist = new People();
-            for(int i = 0; i < pList.Count; ++i) {
-                if(pList[i].Name == name) {
+            for (int i = 0; i < pList.Count; ++i) {
+                if (pList[i].Name == name) {
                     tlist.Add(pList[i]);
                 }
             }
             UpdateDisplay(tlist);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e) {
+            string phone = txtRemove.Text;
+            if (phone == "")
+                return;
+
+            txtRemove.Text = "";
+            txtRemove.Focus();
+
+            for (int i = 0; i < pList.Count; ++i) {
+                if (pList[i].Phone == phone) {
+                    pList.Remove(i);
+                }
+            }
+            UpdateDisplay(pList);
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e) {
+            UpdateDisplay(pList);
         }
 
         private void UpdateDisplay(People lt) {
